4eaa12d8f646626db999b5580e3d20e56536f1cd
Pavan_If_Git <103913087+pavanifgit@users.noreply.github.com>
2022-04-19 15:32:24 +0530

    Add files via upload

---
+++ Sri-Lanka-Brothers-Tour4-lastupload-file-c/assets/js/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+$(".menu-toggle-fr").click(function() {
+    
+          $(".menu-toggle-fr").toggleClass("active");
+    
+          $(".nav-grid-main-con").toggleClass("active");
+    
+        });
+
+
